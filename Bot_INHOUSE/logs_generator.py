99791b936df1757d449fb2c808ca591176895bf1
Micka070702 <mickael.aitelli.7@gmail.com>
2024-02-18 18:53:07 +0100

    new logs

--- Bot_INHOUSE/logs_generator.py
+++ Bot_INHOUSE/logs_generator.py
@@ -3,12 +3,12 @@ import string
 
 def generate_log():
 
-    id_length = random.randint(5, 8)
-    id_chars = string.ascii_letters + string.digits
+    id_length = 2
+    id_chars = string.digits
     log_id = ''.join(random.choice(id_chars) for _ in range(id_length))
 
-    password_length = random.randint(5, 8)
-    password_chars = string.ascii_letters + string.digits
+    password_length = 3
+    password_chars = string.digits
     log_password = ''.join(random.choice(password_chars) for _ in range(password_length))
 
-    return str("ID: RLIH" + str(log_id) + "\nMDP: RLIH" + str(log_password))
+    return str("ID: IH" + str(log_id) + "\nMDP: IH" + str(log_password))
